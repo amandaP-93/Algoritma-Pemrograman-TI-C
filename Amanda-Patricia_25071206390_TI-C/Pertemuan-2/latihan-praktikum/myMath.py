16b133556f675d5250b7502d46703e899b9b2b57
amandaP-93 <amandapatricia0610@gmail.com>
2026-02-12 18:25:23 +0700

    Add all new praktikum files

---
+++ Amanda-Patricia_25071206390_TI-C/Pertemuan-2/latihan-praktikum/myMath.py
@@ -0,0 +1,25 @@
+# Penambahan(a, b)
+#mengembalikan hasil penjumlahan dua bilangan
+
+def tambah(a, b)
+    return a+b
+def kurang(a, b)
+    return a-b
+def kali(a, b)
+    return a*b
+def bagi(a, b)
+    if b==0:
+        hasil="Pembagian tidak dapat dilakukan karena pembagi bernilai 0"
+    else:
+        hasil=a/b
+def mod(a ,b):
+    return a%b
+print(mod(a, b))
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        
+
+
+
